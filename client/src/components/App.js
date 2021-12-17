78da1755869b0a9abf0054be9e45360d4414cb39
Maks Lobarev <simaks147@gmail.com>
2021-12-17 15:08:44 +0300

    project setup

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Container, Row, Col } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <p className={'ppp mt-2'}>Working</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default App;
