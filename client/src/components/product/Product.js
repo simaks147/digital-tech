1001b44d68d92b7ae38d27de778c693eb19a115a
Maks Lobarev <simaks147@gmail.com>
2022-06-10 17:56:02 +0300

    authorize before checkout on product page

--- client/src/components/product/Product.js
+++ client/src/components/product/Product.js
@@ -4,7 +4,9 @@ import {connect} from "react-redux";
 import {
   loadingProductsSelector,
   loadedProductsSelector,
-  productSelector
+  productSelector,
+  orderSelector,
+  tokenSelector
 } from "../../redux/selectors";
 import {Col, Container, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -12,9 +14,11 @@ import {loadProduct, increaseCart} from "../../redux/actions";
 import ProductCarousel from "./productCarousel";
 import ProductTabs from "./productTabs";
 import Loader from "../loader";
+import {BASKET_ROUTE_SHOPPING, LOGIN_ROUTE} from "../../utils/consts";
+import {push} from "connected-react-router";
 
 
-const Product = ({product, increaseCart, loadProduct, loading, loaded}) => {
+const Product = ({product, order, token, increaseCart, push, loadProduct, loading, loaded}) => {
   useEffect(() => {
       loadProduct();
   }, [loadProduct]);
@@ -31,7 +35,11 @@ const Product = ({product, increaseCart, loadProduct, loading, loaded}) => {
             <div className={styles.content}>
               <div className={styles.title}>{product.title}</div>
               <div className={styles.price}>${product.price}</div>
-              <Button className='c-button' onClick={increaseCart}>Buy now!</Button>
+              {
+                order[product.slug]
+                  ? <Button className='c-button2' onClick={() => push(BASKET_ROUTE_SHOPPING)}>In cart</Button>
+                  : <Button className='c-button' onClick={token ? increaseCart : () => push(LOGIN_ROUTE)}>Buy now!</Button>
+              }
             </div>
           </Col>
           <Col className='d-flex align-items-center flex-column-reverse flex-md-row' md={7}>
@@ -48,11 +56,14 @@ const mapStateToProps = (state, props) => ({
   product: productSelector(state, props),
   loading: loadingProductsSelector(state, props),
   loaded: loadedProductsSelector(state, props),
+  order: orderSelector(state, props),
+  token: tokenSelector(state, props)
 })
 
 const mapDispatchToProps = (dispatch, props) => ({
   increaseCart: () => dispatch(increaseCart(props.id)),
-  loadProduct: () => dispatch(loadProduct(props.id))
+  loadProduct: () => dispatch(loadProduct(props.id)),
+  push: (route) => dispatch(push(route))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
