61a5e37f086d6c74643f9a438032daa21e64c718
Maks Lobarev <simaks147@gmail.com>
2022-06-18 15:04:08 +0300

    mobile menu begin

--- client/src/components/categoryList/CategoryList.js
+++ client/src/components/categoryList/CategoryList.js
@@ -4,17 +4,15 @@ import Nav from 'react-bootstrap/Nav'
 import styles from './categoryList.module.css'
 import CategoryItem from "./categoryItem";
 import {categoriesListSelector} from "../../redux/selectors";
-import {Container} from "react-bootstrap";
 
 const CategoryList = ({categories}) => (
-  <div className={styles.section}>
-    <Container>
-      <Nav className={styles.nav}>
-        {
-          categories.map((item, i) => <CategoryItem key={item.slug} item={item}/>)
-        }
-      </Nav>
-    </Container>
+  <div className={styles.main}>
+    <div className={styles.close}></div>
+    <Nav className={styles.nav}>
+      {
+        categories.map((item, i) => <CategoryItem key={item.slug} item={item}/>)
+      }
+    </Nav>
   </div>
 );
 
