42acf1ab253072b211594eb7b205e3888d740760
Maks Lobarev <simaks147@gmail.com>
2022-07-21 13:39:23 +0300

    product item component in admin

--- client/src/components/admin/productsList/ProductsList.js
+++ client/src/components/admin/productsList/ProductsList.js
@@ -7,43 +7,25 @@ import {
   errorProductsSelector,
   loadingProductsSelector,
   productsLimitSelector,
-  productsListSelector, productsPageSelector,
+  productsListSelector,
+  productsPageSelector,
   productsSortSelector,
-  queryParamsSelector,
   totalCountProductsSelector
 } from "../../../redux/selectors";
 import {deleteProduct, loadProductsList} from "../../../redux/actions";
 import Loader from "../../loader";
-import {PRODUCT_ROUTE, ADMIN_PRODUCT_ROUTE} from "../../../utils/consts";
-import ErrorBoundary from "../../ErrorBoundary";
-import {IKImage} from "imagekitio-react";
-import {images as imagesConfig} from "../../../config";
+import {ADMIN_PRODUCT_ROUTE} from "../../../utils/consts";
 import {Link} from "react-router-dom";
-import {ReactComponent as EditIcon} from "../../../icons/edit-icon.svg";
-import {ReactComponent as DeleteIcon} from "../../../icons/delete-icon.svg";
 import Button from "react-bootstrap/Button";
 import ProductFilter from "../../productFilter/ProductFilter";
 import ProductSort from "../../productSort/ProductSort";
 import Pagination from "../../pagination";
+import ProductItem from "./productItem";
 
-const ProductsList = ({
-                        products,
-                        brands,
-                        loadProductsList,
-                        loading,
-                        deleteProduct,
-                        errors,
-                        queryParams,
-                        totalCount,
-                        limit,
-                        limitVariants,
-                        sort,
-                        sortVariants,
-                        page
-                      }) => {
+const ProductsList = ({products, brands, loadProductsList, loading, errors, totalCount, limit, limitVariants, sort, sortVariants, page}) => {
   useEffect(() => {
     loadProductsList(page, limit, sort);
-  }, [loadProductsList, queryParams, totalCount]);
+  }, [loadProductsList, page, limit, sort, totalCount]);
 
   if (errors)
     return <div className={styles.main}>
@@ -76,41 +58,7 @@ const ProductsList = ({
             />
             <div className={styles.list}>
               {
-                products.map(item => {
-                  const {title, slug, images, price, rating} = item;
-                  return <div key={slug} className={styles.item}>
-                    <Row className='align-items-center'>
-                      <Col xs='auto' className={styles.image}>
-                        <Link to={`${PRODUCT_ROUTE}/${slug}`}>
-                          <ErrorBoundary>
-                            <IKImage
-                              urlEndpoint={imagesConfig.urlEndpoint}
-                              path={images[0]}
-                              transformation={[{
-                                height: 60,
-                                width: 60
-                              }]}/>
-                          </ErrorBoundary>
-                        </Link>
-                      </Col>
-                      <Col className={styles.title}>
-                        <Link to={`${PRODUCT_ROUTE}/${slug}`}>{title}</Link>
-                      </Col>
-                      <Col>
-                        <div>{price}</div>
-                      </Col>
-                      <Col>
-                        <div>{rating.overall}</div>
-                      </Col>
-                      <Col xs='auto' className={styles.icons}>
-                        <Link to={`${ADMIN_PRODUCT_ROUTE}/${slug}`}>
-                          <EditIcon/>
-                        </Link>
-                        <DeleteIcon onClick={() => deleteProduct(slug)}/>
-                      </Col>
-                    </Row>
-                  </div>
-                })
+                products.map(item => <ProductItem key={item.slug} product={item}/>)
               }
             </div>
             <Pagination limitVariants={limitVariants}/>
@@ -126,7 +74,6 @@ const mapStateToProps = (state, props) => ({
   loading: loadingProductsSelector(state),
   errors: errorProductsSelector(state),
   brands: brandsListSelector(state),
-  queryParams: queryParamsSelector(state),
   totalCount: totalCountProductsSelector(state),
   limit: productsLimitSelector(state, props),
   sort: productsSortSelector(state, props),
---
+++ client/src/components/admin/productsList/productItem/ProductItem.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styles from "./productItem.module.css";
+import {Col, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {ADMIN_PRODUCT_ROUTE, PRODUCT_ROUTE} from "../../../../utils/consts";
+import ErrorBoundary from "../../../ErrorBoundary";
+import {IKImage} from "imagekitio-react";
+import {images as imagesConfig} from "../../../../config";
+import {ReactComponent as EditIcon} from "../../../../icons/edit-icon.svg";
+import {ReactComponent as DeleteIcon} from "../../../../icons/delete-icon.svg";
+import {connect} from "react-redux";
+import {deleteProduct} from "../../../../redux/actions";
+
+const ProductItem = ({product}) => {
+  const {title, slug, images, price, rating, deleteProduct} = product;
+  return (
+    <div className={styles.main}>
+      <Row className='align-items-center'>
+        <Col xs='auto' className={styles.image}>
+          <Link to={`${PRODUCT_ROUTE}/${slug}`}>
+            <ErrorBoundary>
+              <IKImage
+                urlEndpoint={imagesConfig.urlEndpoint}
+                path={images[0]}
+                transformation={[{
+                  height: 60,
+                  width: 60
+                }]}/>
+            </ErrorBoundary>
+          </Link>
+        </Col>
+        <Col className={styles.title}>
+          <Link to={`${PRODUCT_ROUTE}/${slug}`}>{title}</Link>
+        </Col>
+        <Col>
+          <div>{price}</div>
+        </Col>
+        <Col>
+          <div>{rating.overall}</div>
+        </Col>
+        <Col xs='auto' className={styles.icons}>
+          <Link to={`${ADMIN_PRODUCT_ROUTE}/${slug}`}>
+            <EditIcon/>
+          </Link>
+          <DeleteIcon onClick={() => deleteProduct(slug)}/>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default connect(null, {deleteProduct})(ProductItem);
