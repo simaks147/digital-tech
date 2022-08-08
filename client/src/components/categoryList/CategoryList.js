8cb7a7ef8bd5fbfb4e9a1cf609ff9f63810fa6e8
Maks Lobarev <simaks147@gmail.com>
2022-08-08 14:10:43 +0300

    nav animation by Transition Group start

--- client/src/components/categoryList/CategoryList.js
+++ client/src/components/categoryList/CategoryList.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {connect} from "react-redux";
 import Nav from 'react-bootstrap/Nav'
 import styles from './categoryList.module.css'
@@ -6,36 +6,42 @@ import CategoryItem from "./categoryItem";
 import {activeNavSelector, categoriesListSelector} from "../../redux/selectors";
 import {ReactComponent as CloseIcon} from "../../icons/close-icon.svg";
 import {closeNav} from "../../redux/actions";
-import cn from "classnames";
+import useWindowSize from "../../hooks/use-window-size";
+import {windowWidth} from "../../config";
+import {CSSTransition} from "react-transition-group";
 
 const CategoryList = ({categories, activeNav, closeNav}) => {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const {width} = useWindowSize();
 
   useEffect(() => {
-    window.addEventListener('resize', () => {
-      setWindowWidth(window.innerWidth);
-    });
-  }, []);
+    document.body.style.overflow = width < windowWidth.lg && activeNav ? 'hidden' : 'visible';
+  }, [activeNav, width]);
 
-  useEffect(() => {
-      document.body.style.overflow = (windowWidth < 992) && activeNav ? 'hidden' : 'visible';
-  }, [activeNav, windowWidth]);
+  // if (width < windowWidth.lg && !activeNav) return null;
 
   return (
-    <div className={cn(styles.main, {active: activeNav})}>
-      <div className={styles.close} onClick={closeNav}>
-        <CloseIcon/>
+    <CSSTransition
+      in={activeNav || width >= windowWidth.lg}
+      timeout={300}
+      classNames='nav'
+      mountOnEnter
+      unmountOnExit
+    >
+      <div className={styles.main}>
+        <div className={styles.close} onClick={closeNav}>
+          <CloseIcon/>
+        </div>
+        <Nav className={styles.nav}>
+          {
+            categories.map((item, i) => <CategoryItem key={item.slug} item={item}/>)
+          }
+        </Nav>
       </div>
-      <Nav className={styles.nav}>
-        {
-          categories.map((item, i) => <CategoryItem key={item.slug} item={item}/>)
-        }
-      </Nav>
-    </div>
+    </CSSTransition>
   );
 }
 
-const mapStateToProps = (state, props) => ({
+const mapStateToProps = (state) => ({
   categories: categoriesListSelector(state),
   activeNav: activeNavSelector(state)
 });
