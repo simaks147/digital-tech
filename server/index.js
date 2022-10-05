53df6557a81208d15cb9cec71ce382a07318a4f3
Maks Lobarev <simaks147@gmail.com>
2022-10-06 01:53:36 +0300

    commit

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const config = require('./config');
 const socket = require('./libs/socket');
 
-const server = app.listen(config.port, () => {
+const server = app.listen(config.port, config.host, () => {
   console.log(`App is running on ${config.host}:${config.port}`)
 });
 
