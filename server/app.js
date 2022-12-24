adb6bdead68224524a5d811be47872f965cce36e
Maks Lobarev <simaks147@gmail.com>
2022-12-24 22:54:31 +0300

    remove history middleware

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ const path = require('path');
 const serve = require('koa-static');
 const login = require('./middleware/login');
 const error = require('./middleware/error');
-const history = require('./middleware/history');
 const router = require('./router');
 
 const app = new Koa();
@@ -18,6 +17,5 @@ app.use(serve(path.join(__dirname, '../client/build')));
 app.use(error);
 app.use(login);
 app.use(router.routes());
-app.use(history);
 
 module.exports = app;
