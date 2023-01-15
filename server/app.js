5119adcc5d3f5f49847895d4f8c9894aeabbd39c
Maks Lobarev <simaks147@gmail.com>
2023-01-15 20:00:48 +0300

    return history middleware

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const serve = require('koa-static');
 const login = require('./middleware/login');
 const error = require('./middleware/error');
+const history = require('./middleware/history');
 const router = require('./router');
 
 const app = new Koa();
@@ -17,5 +18,6 @@ app.use(serve(path.join(__dirname, '../client/build')));
 app.use(error);
 app.use(login);
 app.use(router.routes());
+app.use(history);
 
 module.exports = app;
