7e633a0af11aa7aa7b6af20e42c5b010f486562e
Maks Lobarev <simaks147@gmail.com>
2023-08-13 17:26:23 +0300

    types for Loader

--- client/src/components/loader/Loader.tsx
+++ client/src/components/loader/Loader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Spinner from "react-bootstrap/Spinner";
 
 const Loader = () => (
-  <Spinner animation="border" role="status" className='c-loader' type="submit">
+  <Spinner animation="border" role="status" className='c-loader'>
     <span className="visually-hidden">Loading...</span>
   </Spinner>
 );
