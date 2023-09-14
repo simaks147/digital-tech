0b5907296a5ba2a01d17c038d3829bded57d491a
Maks Lobarev <simaks147@gmail.com>
2023-09-14 14:42:58 +0300

    Revert "test"
    This reverts commit 37d60a531333c1358ef576a0fa104f5691b159d3.

--- server/app.js
+++ server/app.js
@@ -2,11 +2,11 @@ const Koa = require('koa');
 const compress = require('koa-compress');
 const cors = require('@koa/cors');
 const bodyparser = require('koa-bodyparser');
-const path = require('path');
-const serve = require('koa-static');
+// const path = require('path');
+// const serve = require('koa-static');
 const login = require('./middleware/login');
 const error = require('./middleware/error');
-const history = require('./middleware/history');
+// const history = require('./middleware/history');
 const router = require('./router');
 
 const app = new Koa();
@@ -14,10 +14,10 @@ const app = new Koa();
 app.use(compress());
 app.use(cors());
 app.use(bodyparser());
-app.use(serve(path.join(__dirname, '../client/build')));
+// app.use(serve(path.join(__dirname, '../client/build')));
 app.use(error);
 app.use(login);
 app.use(router.routes());
-app.use(history);
+// app.use(history);
 
 module.exports = app;
